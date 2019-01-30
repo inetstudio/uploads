4e4e0e174a49edcd8d6d5284d454fda171c76134
Konstantin Bukin <bukin@inetstudio.ru>
2019-01-30 22:26:58 +0300

    Remove logs

--- src/Services/Back/ImagesService.php
+++ src/Services/Back/ImagesService.php
@@ -2,7 +2,6 @@
 
 namespace InetStudio\Uploads\Services\Back;
 
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use InetStudio\Uploads\Contracts\Services\Back\ImagesServiceContract;
 
@@ -24,11 +23,6 @@ class ImagesService implements ImagesServiceContract
     {
         $model = ($model) ? '.'.$model : '';
 
-        if ($model == '.result') {
-            Log::info('Edit result '.$item->id.': '.json_encode(request()->all()));
-            Log::info('Edit result '.$item->id.' media: '.json_encode($item->media->toArray()));
-        }
-
         foreach ($images as $requestName => $name) {
             $properties = (is_numeric($requestName)) ? $request->get($name) : $request->input($requestName);
 
@@ -42,14 +36,8 @@ class ImagesService implements ImagesServiceContract
             ]));
 
             if (isset($properties['has_images']) && ! isset($properties['images'])) {
-                if ($model == '.result') {
-                    Log::info('Edit result '.$item->id.' images 1: '.$name);
-                }
                 $item->clearMediaCollection($name);
             } elseif (isset($properties['images'])) {
-                if ($model == '.result') {
-                    Log::info('Edit result '.$item->id.' images 2: '.$name.' | '.json_encode($properties['images']));
-                }
                 $item->clearMediaCollectionExcept($name, $properties['images']);
 
                 foreach ($properties['images'] as $image) {
@@ -106,9 +94,6 @@ class ImagesService implements ImagesServiceContract
                     $image = $properties['tempname'];
                     $filename = $properties['filename'];
 
-                    if ($model == '.result') {
-                        Log::info('Edit result '.$item->id.' images 3: '.$name);
-                    }
                     $item->clearMediaCollection($name);
 
                     array_forget($properties, ['tempname', 'temppath', 'filepath', 'filename']);
